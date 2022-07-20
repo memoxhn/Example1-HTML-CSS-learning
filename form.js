be6794067fcd1c906d435bab822cd4eed5a3fc1f
Melvin Rosales <melvinre1997@gmail.com>
2022-07-19 21:25:23 -0600

    last changes before pr

--- form.js
+++ form.js
@@ -1,4 +1,17 @@
-const submitForm = (event) => {
-  event.preventDefault();
-  alert("hola");
+const submitForm = (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("mail").value;
+  const msg = document.getElementById("msg").value;
+
+  if (!name || !email || !msg) {
+    alert("porfavor llena todos los campos")
+    return;
+  }
+
+  const myObjectToSend = { name: name, email: email, msg: msg, number: 30 }
+
+  console.log(myObjectToSend);
+  console.log(JSON.stringify(myObjectToSend))
 }
