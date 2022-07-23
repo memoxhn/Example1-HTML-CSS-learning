28a23e624a0d28dd794a12c6573b65f4e4aa9090
Melvin Rosales <melvinre1997@gmail.com>
2022-07-23 11:13:43 -0600

    added form controllers

--- form.js
+++ form.js
@@ -6,12 +6,25 @@ const submitForm = (e) => {
   const msg = document.getElementById("msg").value;
 
   if (!name || !email || !msg) {
-    alert("porfavor llena todos los campos")
+    alert("porfavor llena todos los campos");
     return;
   }
 
-  const myObjectToSend = { name: name, email: email, msg: msg, number: 30 }
+  const myObjectToSend = { name: name, email: email, msg: msg, number: 30 };
 
   console.log(myObjectToSend);
-  console.log(JSON.stringify(myObjectToSend))
-}
+  console.log(JSON.stringify(myObjectToSend));
+};
+
+const getSelectedValue = () => {
+  const selectedDropdown = document.getElementById("ddlViewBy");
+  alert(selectedDropdown.value);
+
+  const selectedValue =
+    selectedDropdown.options[selectedDropdown.selectedIndex].text;
+  alert(selectedValue);
+};
+
+getSelectedValueFromRadioButtons = () => {
+  alert(document.querySelector('input[name="rate"]:checked').value);
+};
